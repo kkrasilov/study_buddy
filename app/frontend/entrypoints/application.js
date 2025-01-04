96b46ba1ddfc1786b3d0a2117972590e5c5213ba
kkrasilov <kkrasilov@gmail.com>
2025-01-05 00:29:29 +0500

    add preline

--- app/frontend/entrypoints/application.js
+++ app/frontend/entrypoints/application.js
@@ -5,6 +5,7 @@ console.log('123')
 import './application.scss'
 import toast from './toast'
 import 'trix'
+import 'preline'
 import '@rails/actiontext'
 
 window.toast = toast;
--- config/deploy.rb
+++ config/deploy.rb
@@ -9,11 +9,10 @@ set :domain, '82.97.253.42'
 set :user, 'deploy'
 set :deploy_to, '/var/www/study_buddy'
 set :repository, 'https://github.com/kkrasilov/study_buddy.git'
-set :branch, 'develop'
+set :branch, 'add-vite'
 
 set :shared_dirs, fetch(:shared_dirs, []).push(
-  'storage',
-  'public'
+  'storage'
 )
 
 set :shared_files, fetch(:shared_files, []).push(
@@ -25,6 +24,11 @@ set :shared_files, fetch(:shared_files, []).push(
 
 task :remote_environment do
   invoke :'rvm:use', 'ruby-3.2.2@default'
+  invoke :'nvm:load'
+end
+
+task :setup do
+  invoke :'nvm'
 end
 
 desc 'Deploys the current version to the server.'
@@ -33,8 +37,11 @@ task :deploy do
     invoke :'git:clone'
     invoke :'deploy:link_shared_paths'
     invoke :'bundle:install'
+    invoke :'nvm:install'
     invoke :'rails:db_migrate'
-    invoke :'rails:assets_precompile'
+    command %(yarn install --ignore-engines)
+    command %(RAILS_ENV=production bin/vite clobber)
+    command %(RAILS_ENV=production bin/vite build)
     invoke :'deploy:cleanup'
 
     on :launch do
@@ -43,5 +50,33 @@ task :deploy do
         command %(touch tmp/restart.txt)
       end
     end
+  end
+end
+
+task :nvm do
+  command 'echo "-----> Installing Node Version Manager"'
+  command 'curl -s https://raw.github.com/creationix/nvm/master/install.sh | sh'
+  invoke :'nvm:load'
+end
+
+task :bundle do
+  command 'bundle install'
+end
+
+namespace :nvm do
+  task :load do
+    command 'echo "-----> Loading nvm"'
+    command %{
+      source ~/.nvm/nvm.sh
+    }
+    command 'echo "-----> Now using nvm v.`nvm --version`"'
+  end
+
+  task :install do
+    command %{
+      echo "-----> Install node v.#{'16'}"
+      nvm install #{'16'}
+      ln -s ${NVM_BIN} ./.bin
+    }
   end
 end
