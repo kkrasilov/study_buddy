2751247b2947747f51fa0903539c564f8e5ea40e
kkrasilov <kkrasilov@gmail.com>
2025-01-04 14:33:04 +0500

    add landing page

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,16 @@ Rails.application.routes.draw do
   devise_for :users
   get '/guests', to: redirect('/guests.html')
 
-  root 'welcome#index'
+  root 'groups#index'
+
+  resources :welcome, only: :index
+  resources :users, only: %i[edit update] do
+    get :profile, on: :collection
+  end
+
+  namespace :api do
+    namespace :v1 do
+      resources :countries, only: [:index]
+    end
+  end
 end
