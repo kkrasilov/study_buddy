5f3a55e62e55cb0154ed121c7f45a1e9ee3d9a4a
kkrasilov <kkrasilov@gmail.com>
2025-01-04 14:37:03 +0500

    add js preline

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,4 +7,4 @@ pin '@hotwired/turbo-rails', to: 'turbo.min.js'
 pin '@hotwired/stimulus', to: 'stimulus.min.js'
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js'
 pin_all_from 'app/javascript/controllers', under: 'controllers'
-pin 'toastify-js' # @1.12.0
+pin 'preline', to: 'https://cdn.jsdelivr.net/npm/preline@2.7.0/+esm'
